df3a5d068cf16a9a2bf3e6146a5cb7f3bc955222
ChelyYi <197163391@qq.com>
2020-02-25 22:50:01 +0100

    faster and better version, approximate comparison and heuristic operator

--- src/operators.py
+++ src/operators.py
@@ -1,94 +1,127 @@
-import math
-import time
-import random
 import copy
 
 import numpy as np
 from sko.ACA import ACA_TSP
 
-def create_newtrip(trip, vertex_num, distance_matrix):
+
+def create_newtrip(trip, vertex_num, distance_matrix, heuristic_matrix, release_date):
     # update current trip, using some operator
     # 1. multiple operator: vertex relocation, depot shift, depot insertion, depot removal √
     # 2. ruin and recreate
 
-    if len(trip) > vertex_num * 0.7:
-        prob = [0.25, 0.25, 0.5]
+    if len(trip) == 1:
+        prob = [0, 1, 0, 0]
+    elif len(trip) >= vertex_num - 1:
+        prob = [0, 0, 0.5, 0.5]
     elif len(trip) <= 2:
-        prob = [0.3, 0.4, 0.3]
+        prob = [0.3, 0.5, 0.1, 0.1]
+    elif len(trip) > vertex_num * 0.6:
+        prob = [0.2, 0.2, 0.3, 0.3]
     else:
-        prob = None
+        prob = [0.3, 0.3, 0.1, 0.3]
 
-    operator = np.random.choice([1, 2, 3], size=1, p=prob)[0]
+    operator = np.random.choice([1, 2, 3, 4], size=1, p=prob)[0]
     new_trip = copy.deepcopy(trip)
-    if operator == 1:  # relocate a vertex to another route contains neighbor
-        new_trip = vertex_relocate(new_trip, vertex_num, distance_matrix)
-    elif operator == 2:  # insert a depot in a route
-        new_trip = route_split(new_trip, distance_matrix, vertex_num)
-    elif operator == 3:  # remove a depot, combine two route
-        new_trip = route_combine(new_trip, distance_matrix)
+    if operator == 1:
+        new_trip = vertex_relocate(new_trip, vertex_num, heuristic_matrix)
+    elif operator == 2:
+        new_trip = route_split(new_trip, vertex_num, heuristic_matrix)
+    elif operator == 3:
+        new_trip = merge_single(new_trip, heuristic_matrix)
+    elif operator == 4:
+        new_trip = route_combine(new_trip, heuristic_matrix)
 
     return new_trip
 
 
-def vertex_relocate(trip, vertex_num, distance_matrix):
+def vertex_relocate(trip, vertex_num, heuristic_marix):
     if len(trip) <= 1:
-        return trip
+        return route_split(trip, vertex_num, heuristic_marix)
+
     vertex = np.random.randint(1, vertex_num)
+    ordered = list(np.argsort(heuristic_marix[vertex]))
+    ordered.remove(0)
+    neighbors_time = ordered[:3]
+
+    records = []  # (route_index,neighbor_index)
     origin_route = 0
-    # vertex_dis = distance_matrix[vertex]
-    # neighbors = vertex_dis.argsort()[-int(round(0.5 * vertex_num)):]
-    # possible_routes = []  # (route, neighbor)
     for i in range(len(trip)):
         route = trip[i]
         if vertex in route:
             origin_route = i
-            if len(route) <= 1:
-                del trip[i]
-            else:
-                route.remove(vertex)  # mutable
-                trip[origin_route] = single_route_optimize(trip[origin_route], distance_matrix)
-            break
-
-    des_route = np.random.randint(0, len(trip))
-    if des_route == origin_route:
-        des_route = np.random.randint(0, len(trip))
-    trip[des_route].append(vertex)
-    # optimize changed route
-    trip[des_route] = single_route_optimize(trip[des_route], distance_matrix)
+            # trip[origin_route] = single_route_optimize(trip[origin_route], distance_matrix)
+            continue
+        else:
+            for n in range(len(neighbors_time)):
+                if neighbors_time[n] in route:
+                    records.append((i, n))
+                    break  # find closest neighbor in this route
+    if len(records) > 0:
+        records.sort(key=lambda tup: tup[1])
+        des = records[0]
+        trip[des[0]].append(vertex)
+        trip[origin_route].remove(vertex)
+        trip = list(filter(lambda r: r != [], trip))  # remove empty
+    else:
+        trip = route_split(trip, vertex_num, heuristic_marix)
     return trip
 
-def route_split(trip, distance_matrix, vertex_num):
-    # TODO: heuristic: remove latest k as new trip
-    if len(trip) >= vertex_num:
-        return trip
+
+def route_split(trip, vertex_num, heuristic_matrix):
+    if len(trip) >= vertex_num - 1:
+        return merge_single(trip, heuristic_matrix)
+
+    # split a random route
     rand_index = np.random.randint(0, len(trip))
     while len(trip[rand_index]) <= 1:
         rand_index = np.random.randint(0, len(trip))
-    rand_route = trip[rand_index]
-    # choose a random position to split the route
-    rand_pos = np.random.randint(1, len(rand_route))
-    sub_route = rand_route[rand_pos:]  # second part
-    sub_route = single_route_optimize(sub_route, distance_matrix)
+    route = trip[rand_index]
+    vertex_index = np.random.randint(0, len(route))  # random choose a vertex
+    k = round(len(route) * np.random.rand())  # split portion
+    new_route = [route[vertex_index]]
 
-    # trip[rand_index] = rand_route[:rand_pos]
-    trip[rand_index] = single_route_optimize(rand_route[:rand_pos], distance_matrix)
-    trip.insert(rand_index + 1, sub_route)
-    return trip
+    sub_matrix = heuristic_matrix[route][:, route]
+    for _ in range(k):
+        vertex_index = np.argmax(sub_matrix[vertex_index])
+        new_route.append(route[vertex_index])
 
-def route_combine(trip, distance_matrix):
-    # TODO: heuristic, combine close time or distance route
-    if len(trip) > 1:
-        depot_pos = np.random.randint(1, len(trip))
-        route1 = trip[depot_pos - 1]
-        route2 = trip[depot_pos]
-        # trip[depot_pos - 1] = route1 + route2
-        trip[depot_pos - 1] = single_route_optimize(route1 + route2, distance_matrix)
-        del trip[depot_pos]
+    new_route = list(set(new_route))
+    trip[rand_index] = list(set(route) - set(new_route))
+    trip.insert(rand_index + 1, new_route)
+    trip = list(filter(lambda r: r != [], trip))  # remove empty
     return trip
 
 
-def local_optimize(trip):
-    pass
+def route_combine(trip, heuristic_matrix):
+    # combine by release date or distance
+    route_index = np.random.randint(0, len(trip))
+    route = trip[route_index].copy()
+    for v in route:
+        neighbor = np.argmax(heuristic_matrix[v][1:]) + 1
+        for r in trip:
+            if neighbor in r and r != route:
+                idx = r.index(neighbor)
+                r.insert(idx, v)
+                trip[route_index].remove(v)
+                break
+    trip = list(filter(lambda r: len(r) >= 1, trip))  # remove empty route
+    return trip
+
+
+def merge_single(trip, heuristic_matrix):
+    # merge single vertex route
+    for route in trip:
+        if len(route) <= 1:
+            for v in route:
+                neighbor = np.argmax(heuristic_matrix[v][1:]) + 1
+                for r in trip:
+                    if neighbor in r:
+                        idx = r.index(neighbor)
+                        r.insert(idx, v)
+                        route.remove(v)
+                        break
+    trip = list(filter(lambda r: len(r) >= 1, trip))  # remove empty route
+    return trip
 
 
 def schedule_route(trip, release_date):
@@ -118,10 +151,7 @@ def trip_optimize(trip, distance_matrix, release_date):
         new_route = single_route_optimize(route, distance_matrix)
         optimal_trip.append(new_route)
 
-    optimal_trip = schedule_route(optimal_trip,release_date)
-    print("Old trip:", trip)
-    print("Optimized trip: ", optimal_trip)
-
+    optimal_trip = schedule_route(optimal_trip, release_date)
     return optimal_trip
 
 
@@ -130,35 +160,78 @@ def single_route_optimize(route, distance_matrix):
         num_points, = routine.shape
         return sum([sub_dis_matrix[routine[i % num_points], routine[(i + 1) % num_points]] for i in range(num_points)])
 
-    if len(route) < 2:
+    if len(route) <= 2:
         return route
 
-    sub_dis_matrix = []  # distance matrix for this route vertices
-    route = [0] + route  # add depot to complete whole route
-    for v in route:
-        sub_dis_matrix.append(distance_matrix[v][route])
-    sub_dis_matrix = np.array(sub_dis_matrix)
+    route = [0] + route
+    sub_dis_matrix = distance_matrix[route][:, route]
     num_points = len(route)
-    aca = ACA_TSP(func=cal_distance,n_dim=num_points,size_pop=int(num_points),max_iter=100,distance_matrix=sub_dis_matrix)
+    aca = ACA_TSP(func=cal_distance, n_dim=num_points, size_pop=int(num_points * 2), max_iter=150,
+                  distance_matrix=sub_dis_matrix)
     best_route_index, best_distance = aca.run()
     best_route_index = best_route_index.tolist()
 
     # construct route without depot
     depot_index = best_route_index.index(0)  # 0 is depot in route
-    k = depot_index + 1  # counter
-    new_route = []
-    while k < len(best_route_index):  # add right part
-        new_route.append(route[best_route_index[k]])
-        k += 1
-    if depot_index > 0:  # has left part
-        k = 0
-        while k < depot_index:
-            new_route.append(route[best_route_index[k]])
-            k += 1
-    return new_route
+    route = np.array(route)
+    route = route[best_route_index[depot_index + 1:]].tolist() + route[best_route_index[:depot_index][::-1]].tolist()
+    return route
 
 
-def compare_trip(old_trip, new_trip):
-    # 1. compare roughly, just by starting time saving and detour saving  [faster]
+def fast_optimize_trip(trip, distance_matrix, release_date):
+    optimal_trip = []
+    for r in trip:
+        route = fast_optimize(r, distance_matrix)
+        optimal_trip.append(route)
+    optimal_trip = schedule_route(optimal_trip, release_date)
+    return optimal_trip
 
-    pass
+
+def fast_optimize(route, distance_matrix):
+    # 2-op local optimize
+    if len(route) <= 2:  # at least 3 vertices
+        return route
+    route = [0] + route
+    num_vertex = len(route)
+    maxlen = min(5, int(num_vertex / 2))
+    for l in range(2, maxlen + 1):
+        for start in range(num_vertex):
+            end = (start + l - 1) % num_vertex
+            start1 = (start + num_vertex - 1) % num_vertex
+            end1 = (end + 1) % num_vertex
+            gain = distance_matrix[route[start], route[start1]] + distance_matrix[route[end], route[end1]] - \
+                   distance_matrix[route[start], route[end1]] - distance_matrix[route[end], route[start1]]
+
+            if gain > 0:
+                temp = route[end1]
+                route[end1] = route[start1]
+                route[start1] = temp
+
+    # construct route without depot
+    depot_index = route.index(0)  # 0 is depot in route
+    route = route[depot_index + 1:] + route[:depot_index][::-1]
+    return route
+
+
+def compare_trip(old_trip, new_trip, release_date, vertex_num, distance_matrix):
+    # compare roughly, just by starting time saving and detour saving  [faster]
+    # avg start time
+    # e.g. r1 start_time=5, 3 node; r2 start_time=10, 2 node; then avg start_time=(5*3+10*2)/5 = 7
+    old_sum = sum([release_date[r].max() * len(r) for r in old_trip])
+    new_sum = sum([release_date[r].max() * len(r) for r in new_trip])
+    time_dif = old_sum / vertex_num - new_sum / vertex_num
+
+    # compute route distance
+    def get_distance(trip):
+        total_distance = 0
+        for route in trip:
+            route = [0] + route + [0]  # start and end at depot
+            for k in range(0, len(route) - 1):
+                total_distance += distance_matrix[route[k], route[k + 1]]
+        return total_distance
+
+    old_distance = get_distance(old_trip)
+    new_distance = get_distance(new_trip)
+    distance_dif = old_distance - new_distance
+
+    return time_dif + distance_dif
--- src/tsprd_sa.py
+++ src/tsprd_sa.py
@@ -2,27 +2,29 @@ from scipy import spatial
 
 from src.operators import *
 
+
 def calculate_distance(city_coordinate_matrix):
     # Calculate distances of the paths between each city.
     distance_matrix = spatial.distance.cdist(city_coordinate_matrix, city_coordinate_matrix, metric='euclidean')
     return distance_matrix
 
+
 def load_dataset(file_path):
     city_coordinates = []
     release_date = []
     with open(file_path) as file:
-        for i in range(5): # skip first 5
+        for i in range(5):  # skip first 5
             next(file)
         for line in file:
             strs = line.split("\t")
-            city_coordinates.append([int(strs[0]),int(strs[1])])
+            city_coordinates.append([int(strs[0]), int(strs[1])])
             release_date.append(int(strs[-1]))
 
     num_vertex = len(release_date)
     city_coordinates = np.array(city_coordinates)
     release_date = np.array(release_date)
     distance_matrix = calculate_distance(city_coordinates)
-    return num_vertex,distance_matrix,release_date
+    return num_vertex, distance_matrix, release_date
 
 
 def compute_cost(trip, distance_matrix, release_date):
@@ -48,64 +50,97 @@ def compute_cost(trip, distance_matrix, release_date):
 
 
 def sa_iteration(vertex_num, distance_matrix, release_date):
-    trip = list(range(1,vertex_num))
-    trip = trip_optimize([trip], distance_matrix,release_date)
+    trip = []
+    for i in range(1, vertex_num):
+        trip.append([i])
     trip_cost = compute_cost(trip, distance_matrix, release_date)
-    print("[Initialization] Trip cost: ",trip_cost)
+    print("[Initialization] trip: ", trip)
+    print("[Initialization] Benchmark trip cost: ", trip_cost)
     best_trip = trip
     best_cost = trip_cost
-    best_unchange = 0
+    unchange_times = 0
     T = T0
     count = 0
+    acceptable_criterion = trip_cost * 0.3
     while (T > Ts):
         for i in range(L):
             # early stopping if best solution unchanged long time
-            best_unchange += 1
-            if best_unchange >= Max_unchanged:
-                print("[Early stopping] Best solution not improved in %s iterations." %Max_unchanged)
-                return best_trip,best_cost
+            if unchange_times > Max_unchanged:
+                print("[Early stopping] Solution not improved in %s iterations." % Max_unchanged)
+                best_trip = trip_optimize(best_trip, distance_matrix, release_date)
+                best_cost = compute_cost(best_trip, distance_matrix, release_date)
+                print("[Early stopping] best trip: ", best_trip)
+                print("[Early stopping] best trip cost: ", best_cost)
+                trip = trip_optimize(trip, distance_matrix, release_date)
+                trip_cost = compute_cost(trip, distance_matrix, release_date)
+                print("[Early stopping] end trip: ", trip)
+                print("[Early stopping] end trip cost: ", trip_cost)
+                if trip_cost < best_cost:
+                    best_trip = trip
+                    best_cost = trip_cost
+                return best_trip, best_cost
 
-            new_trip = create_newtrip(trip, vertex_num, distance_matrix)
-            new_trip = schedule_route(new_trip,release_date)
-            cost_new = compute_cost(new_trip,distance_matrix,release_date)
+            new_trip = create_newtrip(trip, vertex_num, distance_matrix, heuristic_matrix, release_date)
+            # new_trip = fast_optimize_trip(new_trip,distance_matrix,release_date)
+            new_trip = schedule_route(new_trip, release_date)
+            print("[Tempareture%.2f Round%s] New Trip: " % (T, count), new_trip)
 
-            print("[Tempareture%.2f Round%s] New Trip cost: " %(T,count),cost_new, "Current trip cost: ",trip_cost)
-            print("[Tempareture%.2f Round%s] New Trip: " %(T,count), new_trip)
-            df = trip_cost - cost_new
+            cost_new = compute_cost(new_trip, distance_matrix, release_date)
+            print("[Tempareture%.2f Round%s] New Trip cost: " % (T, count), cost_new, "Current trip cost: ", trip_cost)
 
-            if df >= 0:  # better trip: always accept
+            df = compare_trip(trip, new_trip, release_date, vertex_num, distance_matrix)  # avg_starttime old-new
+            dif = trip_cost - cost_new
+            if dif >= 0 or df > 0:  # better trip: always accept
                 trip = new_trip
                 trip_cost = cost_new
+                unchange_times = 0
                 if cost_new < best_cost:  # better than current best one
                     best_trip = new_trip
                     best_cost = cost_new
-                    best_unchange = 0
             else:  # worse trip: accept worse trip with a decreasing probability
-                if np.random.rand() <= math.exp(df/T):
+                unchange_times += 1
+                if np.random.rand() <= math.exp(dif / T):
                     trip = new_trip
                     trip_cost = cost_new
-
         T = T * q  # annealing, decreasing temperature
         count += 1
+
+    best_trip = trip_optimize(best_trip, distance_matrix, release_date)
+    best_cost = compute_cost(best_trip, distance_matrix, release_date)
+    print("[End iteration] best trip: ", best_trip)
+    print("[End iteration] best trip cost: ", best_cost)
+    trip = trip_optimize(trip, distance_matrix, release_date)
+    trip_cost = compute_cost(trip, distance_matrix, release_date)
     print("[End iteration] end trip: ", trip)
     print("[End iteration] end trip cost: ", trip_cost)
-    return best_trip,best_cost
+    if trip_cost < best_cost:
+        best_trip = trip
+        best_cost = trip_cost
+    return best_trip, best_cost
 
 
 if __name__ == '__main__':
     T0 = 100  # initial temperature
-    Ts = 0.1  # stop temperature
-    q = 0.7  # annealing, decrease rate
-    L = 20  # link length, iteration number for each temperature
-    Max_unchanged = 150  # number of iteration best solution not change
+    Ts = 0.01  # stop temperature
+    q = 0.9  # annealing, decrease rate
+    L = 200  # link length, iteration number for each temperature
+    Max_unchanged = 300  # number of iteration best solution not change
 
-    vertex_num, distance_matrix, release_date = load_dataset("../datasets/10/C101_1.dat")
+    vertex_num, distance_matrix, release_date = load_dataset("../datasets/20/C101_1.dat")
+    heuristic_matrix = copy.deepcopy(distance_matrix)
+    max_date = release_date.max()
+    for (i, j), x in np.ndenumerate(distance_matrix):
+        if i < j != 0:
+            heuristic_matrix[i, j] = (10 / x) + (max_date - abs(release_date[i] - release_date[j])) / max_date
+        elif i > j:
+            heuristic_matrix[i, j] = heuristic_matrix[j, i]
+        else:
+            heuristic_matrix[i, j] = 0
     start_time = time.time()
     best_trip, best_cost = sa_iteration(vertex_num, distance_matrix, release_date)
     print("-----------------Result--------------------")
     print("Algorithm running %s seconds" % (time.time() - start_time))
 
     print("Best solution, completion time is: ", best_cost)
-    print("Best trip is: ",best_trip)
-    print("Release date: \n",release_date)
-    print("Distance matrix: \n",distance_matrix)
+    print("Best trip is: ",
+          best_trip)  # print("Release date: \n", release_date)  # print("Distance matrix: \n", distance_matrix)
